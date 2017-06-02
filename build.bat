732ff487ca188f8c761a77dbda407a085b99629a
Joe Burns <josebur86@gmail.com>
2017-06-01 21:01:39 -0500

    Update the build for Win32.

--- build.bat
+++ build.bat
@@ -11,7 +11,7 @@ REM Generate Open GL
 cl /nologo /Zi code\generate_gl_init.cpp
 generate_gl_init.exe
 
-cl /nologo /Zi /EHsc code\main.cpp /link /subsystem:CONSOLE third_party\SDL2\lib\x64\SDL2main.lib third_party\SDL2\lib\x64\SDL2.lib opengl32.lib
+cl /nologo /Zi /EHsc /I third_party\SDL2\include code\main.cpp /link /subsystem:CONSOLE third_party\SDL2\lib\x64\SDL2main.lib third_party\SDL2\lib\x64\SDL2.lib opengl32.lib
 
 xcopy /Y third_party\SDL2\lib\x64\SDL2.dll %OutDir%
 
